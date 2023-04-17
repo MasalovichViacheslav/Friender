72c07c2146275ff4de8921ef8fbf4ed74007f002
Slava <Slavutichtich@gmail.com>
2023-04-18 02:06:59 +0300

    Home task 21 - templates

--- friender/arrangement/urls.py
+++ friender/arrangement/urls.py
@@ -3,8 +3,13 @@ from .views import *
 
 # маршутизация
 urlpatterns = [
-    path('now_time/', current_datetime),
-    path('greeting/<str:name>/', greeting),
-    path('class_view/', Example.as_view()),
-    re_path(r'^articles/(?P<year>[0-9]{4})/$', year_archive),
+    # path('now_time/', current_datetime),
+    # path('greeting/<str:name>/', greeting),
+    # path('class_view/', Example.as_view()),
+    # re_path(r'^articles/(?P<year>[0-9]{4})/$', year_archive),
+    path('main/', main, name="main"),
+    path('hosts/', hosts, name="hosts"),
+    path('guests/', guests, name="guests"),
+    path('establishments/', establishments, name="establishments"),
+    path('ratings/', ratings, name="ratings"),
 ]
--- friender/arrangement/views.py
+++ friender/arrangement/views.py
@@ -5,20 +5,59 @@ import datetime
 
 
 # функция представления (вьюшка)
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = f"<html><body>It is now {now}.</body></html>"
-    return HttpResponse(html)
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = f"<html><body>It is now {now}.</body></html>"
+#     return HttpResponse(html)
+#
+#
+# def greeting(request, name):
+#     return HttpResponse(f"<h1>Hello {name}</h1>")
+#
+#
+# def year_archive(request, year):
+#     return HttpResponse(f"headers: {year}")
+#
+#
+# class Example(View):
+#     def get(self,request, *args,**kwargs):
+#         return HttpResponse(f"This is class base view")
 
 
-def greeting(request, name):
-    return HttpResponse(f"<h1>Hello {name}</h1>")
+def main(request):
+    return render(request, "main.html")
+
+HOSTS = {
+    "Phoebe": ["Buffay", 27, "female", 4.3],
+    "Ross": ["Geller", 29, "male", 4.4],
+    "Chandler": ["Bing", 27, "male", 4.6]
+}
+
+def hosts(request):
+    return render(request, "hosts.html", context={"HOSTS": HOSTS})
 
 
-def year_archive(request, year):
-    return HttpResponse(f"headers: {year}")
+GUESTS = {
+    "Rachel": ["Green", 25, "female", 4.9],
+    "Monica": ["Geller", 26, "female", 4.6],
+    "Joey": ["Tribbiani", 25, "female", 4.9]
+}
+
+def guests(request):
+    return render(request, "guests.html", context={"GUESTS": GUESTS})
 
 
-class Example(View):
-    def get(self,request, *args,**kwargs):
-        return HttpResponse(f"This is class base view")
+Establishments = {
+    "CUPRUM": ["Revolyutsionnaya str., 2", "5-15 BYN", 4.8],
+    "Elementum": ["Internacionalnaya str., 20A", "35-65 BYN", 4.9],
+    "Cripta": ["Tolbuhina str., 18/2", "65-95 BYN", 4.9],
+    "Bar StandUp": ["Partizanski av., 12", "15-35 BYN", 4.9],
+    "Gambrinus": ["Svobody sq., 2", "15-35 BYN", 5.0],
+    "Duduk": ["Bogdanovicha str., 7", "15-35 BYN", 5.0]
+}
+def establishments(request):
+    return render(request, "establishments.html", context={"ESTABLISHMENTS": Establishments})
+
+
+def ratings(request):
+    return render(request, "ratings.html", context={"ELEMENTS": [HOSTS, GUESTS, Establishments]})
